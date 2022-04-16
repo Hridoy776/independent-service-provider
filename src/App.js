4569662eb17905b6b09f1f23ca6ce6e8b8c111ee
Hridoy <rakibhasanhridoy776@gmail.com>
2022-04-17 01:10:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import AboutMe from './Pages/AboutMe/AboutMe';
+import Blog from './Pages/Blog/Blog';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/home' element={<Home></Home>}/>
+        <Route path='/aboutme' element={<AboutMe></AboutMe>}/>
+        <Route path='/blog' element={<Blog></Blog>}/>
+        <Route path='/login' element={<Login></Login>}/>
+        <Route path='*' element={<NotFound></NotFound>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div>
+            <p>about me</p>
+        </div>
+    );
+};
+
+export default AboutMe;
